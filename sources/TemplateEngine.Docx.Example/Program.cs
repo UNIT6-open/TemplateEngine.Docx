b211dddf94347ec495d90e1adf11aad97acbfa59
aivolkov <me@aivolkov.ru>
2013-06-09 17:00:22 +0600

    Simple example with document template processing

---
+++ sources/TemplateEngine.Docx.Example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TemplateEngine.Docx.TemplateCustomContent;
+
+namespace TemplateEngine.Docx.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valuesToFill = new Content
+            {
+                Tables = new List<TableContent>
+                {
+                    new TableContent 
+                    {
+                        Name = "Team Members",
+                        Rows = new List<TableRowContent>
+                        {
+                            new TableRowContent
+                            {
+                                Fields = new List<FieldContent>
+                                    {
+                                        new FieldContent { Name = "Name", Value = "Eric" },
+                                        new FieldContent { Name = "Title", Value = "Program Manager" }
+                                    }
+                            },
+                            new TableRowContent
+                            {
+                                Fields = new List<FieldContent>
+                                    {
+                                        new FieldContent { Name = "Name", Value = "Bob" },
+                                        new FieldContent { Name = "Title", Value = "Developer" }
+                                    }
+                            },
+                        }
+                    }
+                }
+            };
+
+            File.Copy("InputTemplate.docx", "OutputDocument.docx");
+            new TemplateProcessor("OutputDocument.docx")
+                .FillContent(valuesToFill)
+                .SaveChanges();
+        }
+    }
+}
