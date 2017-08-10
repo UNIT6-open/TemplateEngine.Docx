cbd1b83f1ff7ae70d32da0e340427b92f93da057
abatar1 <evgenyhalzov>
2017-08-10 11:32:54 +0500

    Added hide example

--- sources/TemplateEngine.Docx.Example/Program.cs
+++ sources/TemplateEngine.Docx.Example/Program.cs
@@ -34,19 +34,19 @@ namespace TemplateEngine.Docx.Example
 		            .AddRow(
 		                new FieldContent("Name", "Bob"),
 		                new FieldContent("Role", "Developer")),
-		        // Add field inside table that not to propagate.
-		        new FieldContent("Count", "2"),
+                // Add field inside table that not to propagate.
+                new FieldContent("Count", "2"),
 		        // Add list.	
 		        new ListContent("Team Members List")
-		            .AddItem(
+                    .AddItem(
 		                new FieldContent("Name", "Eric"),
 		                new FieldContent("Role", "Program Manager"))
 		            .AddItem(
 		                new FieldContent("Name", "Bob"),
 		                new FieldContent("Role", "Developer")),
 
-		        // Add nested list.	
-		        new ListContent("Team Members Nested List")
+                // Add nested list.	
+                new ListContent("Team Members Nested List")
 		            .AddItem(new ListItemContent("Role", "Program Manager")
 		                .AddNestedItem(new FieldContent("Name", "Eric"))
 		                .AddNestedItem(new FieldContent("Name", "Ann")))
@@ -156,60 +156,88 @@ namespace TemplateEngine.Docx.Example
 		                new FieldContent("Born", new DateTime(1856, 7, 10).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Tesla.jpg")),
 		                new FieldContent("Info",
-		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
+		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, " +
+		                    "and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
 		            .AddRow(new FieldContent("Name", "Thomas Edison"),
 		                new FieldContent("Born", new DateTime(1847, 2, 11).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Edison.jpg")),
 		                new FieldContent("Info",
-		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
+		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, " +
+		                    "including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
 		            .AddRow(new FieldContent("Name", "Albert Einstein"),
 		                new FieldContent("Born", new DateTime(1879, 3, 14).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Einstein.jpg")),
 		                new FieldContent("Info",
-		                    "German-born theoretical physicist. He developed the general theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').")),
+		                    "German-born theoretical physicist. He developed the general theory of relativity, " +
+		                    "one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. " +
+		                    "Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').")),
 
 
 		        // Add images inside a list
 		        new ListContent("Scientists List")
 		            .AddItem(new FieldContent("Name", "Nicola Tesla"),
+		                new FieldContent("Dates of life", new DateTime(1856, 7, 10).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Tesla.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1856, 1943)),
 		                new FieldContent("Info",
-		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
+		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, " +
+		                    "and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
 		            .AddItem(new FieldContent("Name", "Thomas Edison"),
+		                new FieldContent("Dates of life", new DateTime(1847, 2, 11).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Edison.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1847, 1931)),
 		                new FieldContent("Info",
-		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
+		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, " +
+		                    "including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
 		            .AddItem(new FieldContent("Name", "Albert Einstein"),
+		                new FieldContent("Dates of life", new DateTime(1879, 3, 14).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Einstein.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1879, 1955)),
 		                new FieldContent("Info",
-		                    "German-born theoretical physicist. He developed the general theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').")),
+		                    "German-born theoretical physicist. He developed the general theory of relativity, " +
+		                    "one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. " +
+		                    "Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').")),
 
-		        new RepeatContent("Repeats")
+                new RepeatContent("Repeats")
 		            .AddItem(new FieldContent("Name", "Nicola Tesla"),
+		                new FieldContent("Dates of life", new DateTime(1856, 7, 10).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Tesla.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1856, 1943)),
 		                new FieldContent("Info",
-		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
+		                    "Serbian American inventor, electrical engineer, mechanical engineer, physicist, " +
+		                    "and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
 		            .AddItem(new FieldContent("Name", "Thomas Edison"),
+		                new FieldContent("Dates of life", new DateTime(1847, 2, 11).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Edison.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1847, 1931)),
 		                new FieldContent("Info",
-		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
+		                    "American inventor and businessman. He developed many devices that greatly influenced life around the world, " +
+		                    "including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
 		            .AddItem(new FieldContent("Name", "Albert Einstein"),
+		                new FieldContent("Dates of life", new DateTime(1879, 3, 14).ToShortDateString()),
 		                new ImageContent("Photo", File.ReadAllBytes("Einstein.jpg")),
-		                new FieldContent("Dates of life", string.Format("{0}-{1}",
-		                    1879, 1955)),
 		                new FieldContent("Info",
-		                    "German-born theoretical physicist. He developed the general theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation')."))
-		    );
+		                    "German-born theoretical physicist. He developed the general theory of relativity, " +
+		                    "one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science. " +
+		                    "Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').")),
+
+                new RepeatContent("Repeats with hide")
+                    .AddItem(new FieldContent("Name", "Nicola Tesla"),
+                        new ImageContent("Photo", File.ReadAllBytes("Tesla.jpg")).Hide(),
+                        new FieldContent("Dates of life", string.Format("{0}-{1}",
+                            1856, 1943)),
+                        new FieldContent("Info",
+                            "Serbian American inventor, electrical engineer, mechanical engineer, physicist, and futurist best known for his contributions to the design of the modern alternating current (AC) electricity supply system"))
+                    .AddItem(new FieldContent("Name", "Thomas Edison"),
+                        new ImageContent("Photo", File.ReadAllBytes("Edison.jpg")),
+                        new FieldContent("Dates of life", string.Format("{0}-{1}",
+                            1847, 1931)).Hide(),
+                        new FieldContent("Info",
+                            "American inventor and businessman. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb."))
+                    .AddItem(new FieldContent("Name", "Albert Einstein"),
+                        new ImageContent("Photo", File.ReadAllBytes("Einstein.jpg")),
+                        new FieldContent("Dates of life", string.Format("{0}-{1}",
+                            1879, 1955)),
+                        new FieldContent("Info",
+                            "German-born theoretical physicist. He developed the general theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics). " +
+                            "Einstein's work is also known for its influence on the philosophy of science. Einstein is best known in popular culture for " +
+                            "his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').").Hide())
+                    );
 
             using (var outputDocument = new TemplateProcessor("OutputDocument.docx")
                 .SetRemoveContentControls(true))
